da7df9f2c1d3f0d63b02622c8fe54f5856590433
AJ Dunn <andrewjamesdunn@gmail.com>
2023-12-19 18:51:19 +1030

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,8 +45,9 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-//    public function canAccessPanel(Panel $panel): bool
-//    {
-//        return $this->hasVerifiedEmail();
-//    }
+   public function canAccessPanel(Panel $panel): bool
+   {
+       return true;
+       //return $this->hasVerifiedEmail();
+   }
 }
